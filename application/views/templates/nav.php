2ed147a89d257eae83c6fedce42a45726f84f36f
Ak-0 <akhos007@fiu.edu>
2017-11-02 06:11:26 +0000

    navigation update.

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -5,9 +5,9 @@
     <div class="row">
 	<div class="navigation">
 		<ul style=" margin: 0px;display: flex;padding: 0px;">
-  <a href='/news'> <li class="<?php if($this->uri->segment(1)=="news"){echo "active";}?>">Home</li></a>
-  <a href='/works'> <li class="<?php if($this->uri->segment(1)=="works"){echo "active";}?>">Works</li></a>
-  <a href='/posts'> <li class="<?php if($this->uri->segment(1)=="posts"){echo "active";}?>" style="">Posts</li></a>
+  <a href='/news'> <li class="<?php if($this->uri->segment(1)=="news"){echo "active";}?>">News</li></a>
+  <a href='/works'> <li class="<?php if($this->uri->segment(1)=="works"){echo "active";}?>">Info</li></a>
+  <a href='/posts'> <li class="<?php if($this->uri->segment(1)=="posts"){echo "active";}?>" style="">Updates</li></a>
   <a href='/contact'> <li class="<?php if($this->uri->segment(1)=="contact"){echo "active";}?>">Contact</li>  </a>
   
   </ul></div>
