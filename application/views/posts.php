d1d2ea239a76436863aab1d3365c3c3ab6fd71bf
Ak-0 <akhos007@fiu.edu>
2017-11-10 13:06:12 +0000

    Changing code for posts page

--- application/views/posts.php
+++ application/views/posts.php
@@ -11,32 +11,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   
 		<h5>Posts</h5>		<div class="main eleven columns">
 
-		 GitHub Updates:
+		<a href="https://github.com/Ak-0/CI-Website"> GitHub Updates:</a>
 		 <hr style="border-color: #474747;margin: 0px 0 20px 0px;width: 20%;">
 <?php
+$updates = json_decode (json_encode ($commits), FALSE);
+foreach($updates as $commit)
+{
+	echo(date('M-d',strtotime($commit->commit->author->date)));
+	if(isset($commit->author)){	echo('<p>'.$commit->author->login.' ');}
+	echo(' <a href ="'.$commit->html_url.'">'.$commit->commit->message.'</a></p>');
+}
 
-#var_dump($commits);
-foreach ($commits as $it=>$item){
-	if (is_array($item)){
-		foreach ($item as $it2=>$item2){
-				if (is_array($item2)){
-					if(array_key_exists('author', $item2)){
-					#	var_dump($item2);
-						foreach ($item2 as $it3=>$item3){
-							if($it3 == 'author'){
-								echo "<p>".$item3['date']." ";
-							}
-							if($it3 == 'message'){
-								echo "<b>".$item3 ."</b></p>";
-							}
 
-						
-						}
-		};
-		};
-		};
-	};
-};
+
+
+
+
 
 	
 
